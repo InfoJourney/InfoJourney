86d68249069f8f5f242a23330d7a926d0e35f872
Fernaoliv <infojourneypro@gmail.com>
2023-11-09 18:09:54 -0300

    animer

--- InfoJourney/script.js
+++ InfoJourney/script.js
@@ -21,3 +21,19 @@ function toggleMenu() {
       }
       prevScrollpos = currentScrollPos;
     }
+    
+    let count = 1
+    document.getElementById('radio1').checked = true
+  
+    setInterval( function(){
+     nextImage()
+    }, 5000)
+  
+    function nextImage() {
+      count++
+      if (count>5){
+        count = 1
+      }
+  
+      document.getElementById('radio'+count).checked = true
+    }
