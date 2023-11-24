cf4507633f10e736c335356f2f01274b094f19c2
InfoJourney <infojourneypro@gmail.com>
2023-11-24 10:36:08 -0300

    tudo de hoje

--- InfoJourney/paginas/card.js
+++ InfoJourney/paginas/card.js
@@ -1,60 +1,60 @@
 const abrir = document.querySelector("#cvs")
 const modal = document.querySelector("#infoCvs")
-const fechar = document.querySelector("#cvs")
+const fechar = document.querySelector("#btn-cvs")
 abrir.onmouseenter = function (){
     modal.showModal()
     
 }
-fechar.onmouseout = function () {
-    modal.close ()
-   
+fechar.onclick = function () {
+    modal.close()
 }
+
 
 
 const abrir2 = document.querySelector("#cvs2")
 const modal2 = document.querySelector("#infoCvs2")
-const fechar2 = document.querySelector("#cvs2")
+const fechar2 = document.querySelector("#btn-cvs2")
 
 abrir2.onmouseenter = function (){
     modal2.showModal()
 }
 
-fechar2.onmouseout = function () {
+fechar2.onclick = function () {
     modal2.close()
 }
 
 const abrir3 = document.querySelector("#cvs3")
 const modal3 = document.querySelector("#infoCvs3")
-const fechar3 = document.querySelector("#cvs3")
+const fechar3 = document.querySelector("#btn-cvs3")
 
 abrir3.onmouseenter = function (){
     modal3.showModal()
 }
 
-fechar3.onmouseout = function () {
+fechar3.onclick = function () {
     modal3.close()
 }
 
 const abrir4 = document.querySelector("#cvs4")
 const modal4 = document.querySelector("#infoCvs4")
-const fechar4 = document.querySelector("#cvs4")
+const fechar4 = document.querySelector("#btn-cvs4")
 
 abrir4.onmouseenter = function (){
     modal4.showModal()
 }
 
-fechar4.onmouseout = function () {
+fechar4.onclick = function () {
     modal4.close()
 }
 
 const abrir5 = document.querySelector("#cvs5")
 const modal5 = document.querySelector("#infoCvs5")
-const fechar5 = document.querySelector("#cvs5")
+const fechar5 = document.querySelector("#btn-cvs5")
 abrir5.onmouseenter = function (){
     modal5.showModal()
     
 }
-fechar5.onmouseout = function () {
+fechar5.onclick = function () {
     modal5.close ()
    
 }
@@ -62,48 +62,48 @@ fechar5.onmouseout = function () {
 
 const abrir6 = document.querySelector("#cvs6")
 const modal6 = document.querySelector("#infoCvs6")
-const fechar6 = document.querySelector("#cvs6")
+const fechar6 = document.querySelector("#btn-cvs6")
 
 abrir6.onmouseenter = function (){
     modal6.showModal()
 }
 
-fechar6.onmouseout = function () {
+fechar6.onclick = function () {
     modal6.close()
 }
 
 const abrir7 = document.querySelector("#cvs7")
 const modal7 = document.querySelector("#infoCvs7")
-const fechar7 = document.querySelector("#cvs7")
+const fechar7 = document.querySelector("#btn-cvs7")
 
 abrir7.onmouseenter = function (){
     modal7.showModal()
 }
 
-fechar7.onmouseout = function () {
+fechar7.onclick = function () {
     modal7.close()
 }
 
 const abrir8 = document.querySelector("#cvs8")
 const modal8 = document.querySelector("#infoCvs8")
-const fechar8 = document.querySelector("#cvs8")
+const fechar8 = document.querySelector("#btn-cvs8")
 
 abrir8.onmouseenter = function (){
     modal8.showModal()
 }
 
-fechar8.onmouseout = function () {
+fechar8.onclick = function () {
     modal8.close()
 }
 
 const abrir9 = document.querySelector("#cvs9")
 const modal9 = document.querySelector("#infoCvs9")
-const fechar9 = document.querySelector("#cvs9")
+const fechar9 = document.querySelector("#btn-cvs9")
 abrir9.onmouseenter = function (){
     modal9.showModal()
     
 }
-fechar9.onmouseout = function () {
+fechar9.onclick = function () {
     modal9.close ()
    
 }
@@ -111,280 +111,279 @@ fechar9.onmouseout = function () {
 
 const abrir10 = document.querySelector("#cvs10")
 const modal10 = document.querySelector("#infoCvs10")
-const fechar10 = document.querySelector("#cvs10")
+const fechar10 = document.querySelector("#btn-cvs10")
 
 abrir10.onmouseenter = function (){
     modal10.showModal()
 }
 
-fechar10.onmouseout = function () {
+fechar10.onclick = function () {
     modal10.close()
 }
 
 const abrir11 = document.querySelector("#cvs11")
 const modal11 = document.querySelector("#infoCvs11")
-const fechar11 = document.querySelector("#cvs11")
+const fechar11 = document.querySelector("#btn-cvs11")
 
 abrir11.onmouseenter = function (){
     modal11.showModal()
 }
 
-fechar11.onmouseout = function () {
+fechar11.onclick = function () {
     modal11.close()
 }
 
 const abrir12 = document.querySelector("#cvs12")
 const modal12 = document.querySelector("#infoCvs12")
-const fechar12 = document.querySelector("#cvs12")
+const fechar12 = document.querySelector("#btn-cvs12")
 
 abrir12.onmouseenter = function (){
     modal12.showModal()
 }
 
-fechar12.onmouseout = function () {
+fechar12.onclick = function () {
     modal12.close()
 }
 
 const abrir13 = document.querySelector("#cvs13")
 const modal13 = document.querySelector("#infoCvs13")
-const fechar13 = document.querySelector("#cvs13")
+const fechar13 = document.querySelector("#btn-cvs13")
 
 abrir13.onmouseenter = function (){
     modal13.showModal()
 }
 
-fechar13.onmouseout = function () {
+fechar13.onclick = function () {
     modal13.close()
 }
 
 const abrir14 = document.querySelector("#cvs14")
 const modal14 = document.querySelector("#infoCvs14")
-const fechar14 = document.querySelector("#cvs14")
+const fechar14 = document.querySelector("#btn-cvs14")
 
 abrir14.onmouseenter = function (){
     modal14.showModal()
 }
 
-fechar14.onmouseout = function () {
+fechar14.onclick = function () {
     modal14.close()
 }
 
 const abrir15 = document.querySelector("#cvs15")
 const modal15 = document.querySelector("#infoCvs15")
-const fechar15 = document.querySelector("#cvs15")
+const fechar15 = document.querySelector("#btn-cvs15")
 
 abrir15.onmouseenter = function (){
     modal15.showModal()
 }
 
-fechar15.onmouseout = function () {
+fechar15.onclick = function () {
     modal15.close()
 }
 
 const abrir16 = document.querySelector("#cvs16")
 const modal16 = document.querySelector("#infoCvs16")
-const fechar16 = document.querySelector("#cvs16")
+const fechar16 = document.querySelector("#btn-cvs16")
 
 abrir16.onmouseenter = function (){
     modal16.showModal()
 }
 
-fechar16.onmouseout = function () {
+fechar16.onclick = function () {
     modal16.close()
 }
 
 
 const abrir17 = document.querySelector("#cvs17")
 const modal17 = document.querySelector("#infoCvs17")
-const fechar17 = document.querySelector("#cvs17")
+const fechar17 = document.querySelector("#btn-cvs17")
 
 abrir17.onmouseenter = function (){
     modal17.showModal()
 }
 
-fechar17.onmouseout = function () {
+fechar17.onclick = function () {
     modal17.close()
 }
 
 
 const abrir18 = document.querySelector("#cvs18")
 const modal18 = document.querySelector("#infoCvs18")
-const fechar18 = document.querySelector("#cvs18")
+const fechar18 = document.querySelector("#btn-cvs18")
 
 abrir18.onmouseenter = function (){
     modal18.showModal()
 }
 
-fechar18.onmouseout = function () {
+fechar18.onclick = function () {
     modal18.close()
 }
 
 
 const abrir19 = document.querySelector("#cvs19")
 const modal19 = document.querySelector("#infoCvs19")
-const fechar19 = document.querySelector("#cvs19")
+const fechar19 = document.querySelector("#btn-cvs19")
 
 abrir19.onmouseenter = function (){
     modal19.showModal()
 }
 
-fechar19.onmouseout = function () {
+fechar19.onclick = function () {
     modal19.close()
 }
 
 
 const abrir20 = document.querySelector("#cvs20")
 const modal20 = document.querySelector("#infoCvs20")
-const fechar20 = document.querySelector("#cvs20")
+const fechar20 = document.querySelector("#btn-cvs20")
 
 abrir20.onmouseenter = function (){
     modal20.showModal()
 }
 
-fechar20.onmouseout = function () {
+fechar20.onclick = function () {
     modal20.close()
 }
 
 
 const abrir21 = document.querySelector("#cvs21")
 const modal21 = document.querySelector("#infoCvs21")
-const fechar21 = document.querySelector("#cvs21")
+const fechar21 = document.querySelector("#btn-cvs21")
 
 abrir21.onmouseenter = function (){
     modal21.showModal()
 }
 
-fechar21.onmouseout = function () {
+fechar21.onclick = function () {
     modal21.close()
 }
 
 
 const abrir22 = document.querySelector("#cvs22")
 const modal22 = document.querySelector("#infoCvs22")
-const fechar22 = document.querySelector("#cvs22")
+const fechar22 = document.querySelector("#btn-cvs22")
 
 abrir22.onmouseenter = function (){
     modal22.showModal()
 }
 
-fechar22.onmouseout = function () {
+fechar22.onclick = function () {
     modal22.close()
 }
 
 
 const abrir23 = document.querySelector("#cvs23")
 const modal23 = document.querySelector("#infoCvs23")
-const fechar23 = document.querySelector("#cvs23")
+const fechar23 = document.querySelector("#btn-cvs23")
 
 abrir23.onmouseenter = function (){
     modal23.showModal()
 }
 
-fechar23.onmouseout = function () {
+fechar23.onclick = function () {
     modal23.close()
 }
 
 
 const abrir24 = document.querySelector("#cvs24")
 const modal24 = document.querySelector("#infoCvs24")
-const fechar24 = document.querySelector("#cvs24")
+const fechar24 = document.querySelector("#btn-cvs24")
 
 abrir24.onmouseenter = function (){
     modal24.showModal()
 }
 
-fechar24.onmouseout = function () {
+fechar24.onclick = function () {
     modal24.close()
 }
 
 
 const abrir25 = document.querySelector("#cvs25")
 const modal25 = document.querySelector("#infoCvs25")
-const fechar25 = document.querySelector("#cvs25")
+const fechar25 = document.querySelector("#btn-cvs25")
 
 abrir25.onmouseenter = function (){
     modal25.showModal()
 }
 
-fechar25.onmouseout = function () {
+fechar25.onclick = function () {
     modal25.close()
 }
 
 
 const abrir26 = document.querySelector("#cvs26")
 const modal26 = document.querySelector("#infoCvs26")
-const fechar26 = document.querySelector("#cvs26")
+const fechar26 = document.querySelector("#btn-cvs26")
 
 abrir26.onmouseenter = function (){
     modal26.showModal()
 }
 
-fechar26.onmouseout = function () {
+fechar26.onclick = function () {
     modal26.close()
 }
 
 
 const abrir27 = document.querySelector("#cvs27")
 const modal27 = document.querySelector("#infoCvs27")
-const fechar27 = document.querySelector("#cvs27")
+const fechar27 = document.querySelector("#btn-cv")
 
 abrir27.onmouseenter = function (){
     modal27.showModal()
 }
 
-fechar27.onmouseout = function () {
+fechar27.onclick = function () {
     modal27.close()
 }
 
 
 const abrir28 = document.querySelector("#cvs28")
 const modal28 = document.querySelector("#infoCvs28")
-const fechar28 = document.querySelector("#cvs28")
+const fechar28 = document.querySelector("#btn-cvs28")
 
 abrir28.onmouseenter = function (){
     modal28.showModal()
 }
 
-fechar28.onmouseout = function () {
+fechar28.onclick = function () {
     modal28.close()
 }
 
 
 const abrir29 = document.querySelector("#cvs29")
 const modal29 = document.querySelector("#infoCvs29")
-const fechar29 = document.querySelector("#cvs29")
+const fechar29 = document.querySelector("#btn-cvs29")
 
 abrir29.onmouseenter = function (){
     modal29.showModal()
 }
 
-fechar29.onmouseout = function () {
+fechar29.onclick = function () {
     modal29.close()
 }
 
 
 const abrir30 = document.querySelector("#cvs30")
 const modal30 = document.querySelector("#infoCvs30")
-const fechar30 = document.querySelector("#cvs30")
+const fechar30 = document.querySelector("#btn-cvs30")
 
 abrir30.onmouseenter = function (){
     modal30.showModal()
 }
 
-fechar30.onmouseout = function () {
+fechar30.onclick = function () {
     modal30.close()
 }
 
 
 const abrir31 = document.querySelector("#cvs31")
 const modal31 = document.querySelector("#infoCvs31")
-const fechar31 = document.querySelector("#cvs31")
+const fechar31 = document.querySelector("#btn-cvs31")
 
 abrir31.onmouseenter = function (){
     modal31.showModal()
 }
 
-fechar31.onmouseout = function () {
+fechar31.onclick = function () {
     modal31.close()
 }
-
